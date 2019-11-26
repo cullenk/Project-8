37189704342114749c3dd6e9bc76cac7a74729fb
cullenk <45953462+cullenk@users.noreply.github.com>
2019-11-26 12:52:27 -0500

    Modal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 const url = "https://randomuser.me/api/?results=12&nat=us,au,ca,ch,de,gb,fr&inc=name, picture, email, location, phone, dob, nat & noinfo";
+const modalContainer = document.querySelector('.modal-container');
+const employees = [];
+const index = 0;
+const searchField = document.getElementById("search");
 
 function generateData() {
  fetch(url) //fetch takes one argument, the source of where the info is coming from. Stored in url variable...
    .then(response => response.json()) //Anonymous function. Take that information, parse the data into json() format which are name: value pairs.
    .then(data => { //Anonymous function, do something...
-    const employees = data.results; //get the data received, store it in "const results"
-    directory(employees);
+    const results = data.results; //get the data received, store it in "const results"
+    directory(results); // call the directory function, create cards for each employee passed through it
+    employees.push(results); //store the employees returned rom the API in a global variable so we can access them later
    })
    .catch(error => console.log("Oops! Something went wrong.", error)) //If something goes wrong, catch the error and log the message
+
 };
 
 generateData();
@@ -24,9 +30,9 @@ const directory = employees => { //Anonymous function to handle the insertion of
         <img class="card-img" src="${employee.picture.large}" alt="${employee.name.first}'s profile picture">
       </div>
       <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+        <h3 id="name" class="card-name cap">${changeCase(employee.name.first)} ${changeCase(employee.name.last)}</h3>
         <p class="card-text">${employee.email}</p>
-        <p class="card-text cap">${employee.location.city}</p>
+        <p class="card-text cap">${changeCase(employee.location.city)}</p>
       </div>
     </div>
   `;
@@ -40,14 +46,33 @@ const directory = employees => { //Anonymous function to handle the insertion of
 
 };
 
+//English Alhabet characters only
+function changeCase(str) {
+  str = str.replace("ß", "ss");
+  str = str.replace("-", " - ");
+  str = str.replace("/", " - ");
+  str = str.replace("\'", " \' ");
+  str = str.replace(/[éêèë]/gi, "e");
+  str = str.replace(/[äàâ]/gi, "a");
+  str = str.replace(/[öôò]/gi, "o");
+  str = str.replace(/[üûù]/gi, "u");
+  let words = str.split(" ");
+  let newString = [];
+  words.forEach(word => newString += `${word[0].toUpperCase()}${word.slice(1)} `);
+  newString = newString.replace(" - ", "-");
+  newString = newString.replace(" \' ", "\'");
+  return newString;
+}
 
 const modal = employee => {
-  const modalContainer = document.querySelector('.modal-container');
   const dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language); // Formats date depending on users locale.
 
   modalContainer.innerHTML = `
     <div class="modal">
       <div class="modal-info-container">
+        <span class="close"> X </span>
+        <span class="previous"> < </span>
+        <span class="next"> > </span>
         <img class="modal-img" src="${employee.picture.large}" alt="${employee.name.first}'s profile picture">
         <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
         <p class="modal-text">${employee.email}</p>
@@ -62,139 +87,44 @@ const modal = employee => {
   modalContainer.style.display = 'block';
 };
 
+  modalContainer.addEventListener("click", (e) => { //When the user clicks on certain things in the pop-up modal...
+    if(e.target.className === "close") { //If they click on the "x"...
+      modalContainer.style.display = "none"; //Remove the modal display
+    } else if (e.target.className === "previous") { //If they click on the previous arrow
+      index --; //Take the current index value and subtract 1 to go back.
+      employee = employees[index]; //the index value of the currently displayed employee will be stored
+      modal(employee); //Run the created modal function to generate another employee card one index back
+    } else if (e.target.className=== "next") {
+      index ++;//Take the current index value and add 1 to go forward.
+      employee = employees[index];//the index value of the currently displayed employee will be stored
+      modal(employee);//Run the created modal function to generate another employee card one index back
+    }
+});
 
+function search() {
+  let searchValue = searchField.value.toLowerCase();
+  let employeeCard = document.querySelectorAll(".card");
+  let employeeName = document.querySelectorAll(".card-info-container");
+  for (let i = 0; i < employeeCard.length; i++) { //Loop through each card thumbnail
+    let title = employeeName[i].getElementsByTagName("h3")[0]; //Obtain the value found within each card's h3 element, starting at the first one [0 index]
+    let name = title.textContent; //Get the person's name from each card, store it in "name"
+    name = removeAccent(name);
+    if (name.toLowerCase().indexOf(searchValue) > -1) { //If that person's name matches what the user is searching at all, so it returns an index of at least 0 (greater than -1)
+      employeeCard[i].style.display = ""; //Take that person and keep them on the screen
+    } else {
+      employeeCard[i].style.display = "none"; //Or else hide them because they don't match the search
+    }
+  }
+}
 
+// Return the data in the English Alphabet only
+function removeAccent(str) {
+  str = str.replace("ß", "ss");
+  str = str.replace(/[éêèë]/gi, "e");
+  str = str.replace(/[äàâ]/gi, "a");
+  str = str.replace(/[öôò]/gi, "o");
+  str = str.replace(/[üûù]/gi, "u");
+  return str;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // JavaScript
-// const url = "https://randomuser.me/api/?results=12&nat=us,au,ca,ch,de,gb,fr&inc=name, picture, email, location, phone, dob, nat & noinfo";
-// let index = 0;
-// const employees = [];
-// const list = document.getElementById("gallery");
-//
-//
-// // Fetch the employee data from the randomuser API
-// function generateData() {
-//  fetch(url) //fetch takes one argument, the source of where the info is coming from. Stored in url variable...
-//    .then(response => response.json()) //Anonymous function. Take that information, parse the data into json() format.
-//    .then(data => { //Anonymous function, do something...
-//      const results = data.results; //get the data received, store it in "const results"
-//      generateEmployees(results);  // Call the generateEmployees function, pass it the data stored in results variable
-//    })
-//    .catch(error => console.log("Oops! Something went wrong.", error)) //If something goes wrong, catch the error and log the message
-// }
-//
-// //Function to create the actual objects for the data
-// function generateEmployees(data) {
-//   let html = []; //set an empty array
-//   let index = 0; //set the index count at 0
-//   data.map(result => { //map over all of the data results
-//     let name = `${result.name.first} ${result.name.last}`;
-//     let city = result.location.city;
-//     const thumbnail = `
-//       <section class="employee-card" data-index=${index}><img class="employee-img" src=${result.picture.large}>
-//       <div class="card">
-//       <h2>${name}</h2>
-//       <p>${result.email}</p>
-//       <p>${city}</p>
-//       </div>
-//       </section>`; //Format all this HTML markup
-//     html += thumbnail; //Add this markup (stored in "thumbnail") to the html array.
-//
-//     // Generate the Employee objects themselves
-//        const employee = {
-//          index,
-//          name,
-//          city,
-//          phone: result.phone,
-//          email: result.email,
-//          street: `${result.location.street.number} ${result.location.street.name}`,
-//          nationality: result.nat,
-//          zipCode: result.location.postcode,
-//          image: result.picture.large,
-//          birthday: new Date(result.dob.date)
-//        }
-//        index += 1;
-//        employees.push(employee);  // add the employee to the employees array
-//
-//      });
-//
-//      list.innerHTML = html; //Adds all of this to the list variable
-//
-//      // Add click functionality to each card element created in the list/gallery
-//      list.querySelectorAll('.card').forEach((card, index) => {
-//        card.addEventListener('click', () => {
-//          modal(employees[index]);
-//        });
-//      });
-//
-//    }
-//
-// generateData();
-//
-// // CREATING THE POP UP MODAL OVERLAY
-//
-//
-// const modal = employee => {
-//   const modalContainer = document.querySelector('.modal-container');
-// // ​  const dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language); // Formats date depending on users locale.
-//
-//   modalContainer.innerHTML = `<div class="modal">
-//       <div class="modal-info-container">
-//         <img class="modal-img" src="${employee.picture.large}" alt="${employee.name.first}'s profile picture">
-//         <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-//         <p class="modal-text">${employee.email}</p>
-//         <p class="modal-text cap">${employee.location.city}</p><hr>
-//         <p class="modal-text">${employee.phone}</p>
-//         <p class="modal-text cap">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.state} ${employee.location.postcode}</p>
-//         <!-- <p class="modal-text">Birthday: </p> -->
-//       </div>
-//     </div>
-// `;
-//
-//   modalContainer.style.display = 'block';
-// };
+searchField.addEventListener("keyup", (e) => search()); //Attach the search function to the searchField to be called when the user starts typing.
